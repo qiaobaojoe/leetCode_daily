4c34dfb32c59caf1de70a08f43866271c9b4678b
majorjoe <qiaobaocn@gmail.com>
2024-06-21 10:24:29 +0800

    feature:实现不下去了,暂缓一下

--- heap/heap.py
+++ heap/heap.py
@@ -1,5 +1,44 @@
+from typing import List, Optional
+
+
 class MaxHeap:
     """
-    最小堆
+    大顶堆
+    完全二叉树,用数组表示
     """
-    pass
+
+    def __init__(self, nums: List[int]):
+        self.heap_list = nums
+        self.heap_size = len(nums)
+
+    def __init__(self):
+        self.heap_list = []
+        self.heap_size = 0
+
+    def push(self, num: int):
+        pass
+
+    def pop(self) -> int:
+        if self.is_empty():
+            raise ValueError("空堆!!")
+
+    def peek(self) -> Optional[int]:
+        if self.is_empty():
+            return None
+        return self.heap_list[0]
+
+    def size(self) -> int:
+        return self.heap_size
+
+    def is_empty(self) -> bool:
+        return self.heap_size == 0
+
+
+def main():
+    max_heap = MaxHeap()
+    print(max_heap.size())
+    print(max_heap.is_empty())
+
+
+if __name__ == "__main__":
+    main()
