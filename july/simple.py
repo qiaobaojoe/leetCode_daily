2586d65794e3e30bfc342934be1678d4f1a6558e
majorjoe <qiaobaocn@gmail.com>
2024-07-03 10:34:14 +0800

    feature:数字和字符的转换处理

--- july/simple.py
+++ july/simple.py
@@ -40,10 +40,19 @@ class Solution:
                     last = i
         return last - first
 
+    def sum_of_the_digits_of_harshad_number(self, x: int) -> int:
+        str_x = str(x)
+        sum_c = 0
+        for cur in str_x:
+            sum_c += int(cur)
+        if x % sum_c == 0:
+            return sum_c
+        return -1
+
 
 def main():
     solution = Solution()
-    print(solution.maximum_prime_difference([4, 2, 9, 5, 3]))
+    print(solution.sum_of_the_digits_of_harshad_number(18))
 
 
 if __name__ == "__main__":
