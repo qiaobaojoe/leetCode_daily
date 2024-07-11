75859d2b7ee4531c3ace113ad2b08b454efc1489
majorjoe <qiaobaocn@gmail.com>
2024-07-11 17:40:04 +0800

    feature:下标l是项大于目标值的方向移动的，所以没有找到目标值，l就是一个大于目标值的元素下标

--- search/search_solution.py
+++ search/search_solution.py
@@ -16,10 +16,23 @@ class SearchSolution:
                 r = m - 1
         return -1
 
+    def search_insert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return l
+
 
 def main():
     solution = SearchSolution()
-    print(solution.search([-1, 0, 3, 5, 9, 12], 9))
+    print(solution.search_insert([1, 3, 5, 6], 7))
 
 
 if __name__ == "__main__":
