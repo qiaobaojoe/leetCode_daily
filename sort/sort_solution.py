20817d89c1602c49f7157551ee88923a0f091e34
majorjoe <qiaobaocn@gmail.com>
2024-07-12 17:30:20 +0800

    feature:选择排序

---
+++ sort/sort_solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class SortSolution:
+
+    def select_sort(self, nums: List[int]) -> List[int]:
+        for i,cur in enumerate(nums):
+            min_index = i
+            for j in range(i+1,len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            if min_index != i:
+                nums[i] = nums[min_index]
+                nums[min_index] = cur
+        return nums
+
+
+def main():
+    solution = SortSolution()
+    print(solution.select_sort([2, 4, 8, 9]))
+
+
+if __name__ == "__main__":
+    main()
