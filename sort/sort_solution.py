c0fa57ea4437fee3058407fc0851828ac3c21054
majorjoe <qiaobaocn@gmail.com>
2024-07-12 17:43:19 +0800

    feature:优化选择排序

--- sort/sort_solution.py
+++ sort/sort_solution.py
@@ -4,14 +4,13 @@ from typing import List
 class SortSolution:
 
     def select_sort(self, nums: List[int]) -> List[int]:
-        for i,cur in enumerate(nums):
+        for i in range(len(nums) - 1):
             min_index = i
-            for j in range(i+1,len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[j] < nums[min_index]:
                     min_index = j
             if min_index != i:
-                nums[i] = nums[min_index]
-                nums[min_index] = cur
+                nums[i], nums[min_index] = nums[min_index], nums[i]
         return nums
 
 
