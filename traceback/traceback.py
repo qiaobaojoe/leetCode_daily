62f152cfa663b4502c07a3fc61409fd9849e52c7
majorjoe <qiaobaocn@gmail.com>
2024-08-22 17:03:26 +0800

    feature:回溯的结构关键还是理解递归，难点就是在剪枝的设计

--- traceback/traceback.py
+++ traceback/traceback.py
@@ -19,6 +19,7 @@ class Traceback:
     ):
         if 0 == target:
             ans.append(state[::])
+            return
 
         for i in range(start, len(choices)):
             choice = choices[i]
@@ -29,10 +30,42 @@ class Traceback:
             else:
                 break
 
+    def combination_sum_2(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        candidates.sort()
+        self.combination_sum_trace_2([], target, candidates, 0, ans)
+        return ans
+
+    def combination_sum_trace_2(
+        self,
+        state: List[int],
+        target: int,
+        choices: List[int],
+        start: int,
+        ans: List[List[int]],
+    ):
+        if 0 == target:
+            ans.append(state[::])
+            return
+
+        for i in range(start, len(choices)):
+            choice = choices[i]
+            if i > start and choice == choices[i - 1]:
+                continue
+            if target - choice >= 0:
+                state.append(choice)
+                self.combination_sum_trace_2(
+                    state, target - choice, choices, i + 1, ans
+                )
+                state.pop()
+            else:
+                break
+
 
 def main():
     trackback = Traceback()
     print(trackback.combination_sum([2, 3, 6, 7], 7))
+    print(trackback.combination_sum_2([2, 5, 2, 1, 2], 5))
 
 
 if __name__ == "__main__":
