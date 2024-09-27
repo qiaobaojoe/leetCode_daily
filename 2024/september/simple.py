0c9cbe31d8d52b9372fe16d4de5e47b22f8da1d4
majorjoe <qiaobaocn@gmail.com>
2024-09-27 11:32:43 +0800

    feature:简单题就是穷举

--- 2024/september/simple.py
+++ 2024/september/simple.py
@@ -16,10 +16,28 @@ class SimpleSolution:
 
         return abs(sum_num - sum_digit)
 
+    def find_judge(self, n: int, trust: List[List[int]]) -> int:
+        trust_count = [0] * (n + 1)
+        be_trusted_count = [0] * (n + 1)
+
+        for t in trust:
+            a, b = t[0], t[1]
+            trust_count[a] += 1
+            be_trusted_count[b] += 1
+
+        for t_n, t_c in enumerate(trust_count):
+            if t_n == 0:
+                continue
+            if t_c == 0:
+                if be_trusted_count[t_n] == n - 1:
+                    return t_n
+
+        return -1
+
 
 def main():
     solution = SimpleSolution()
-    print(solution.difference_of_sum([1, 15, 6, 3]))
+    print(solution.find_judge(2, [[1, 2]]))
     # print(9 // 10)
     # print(9 % 10)
 
