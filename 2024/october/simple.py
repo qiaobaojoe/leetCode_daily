69393e2e9e29f9ceacab5ebabab257fa506cef56
majorjoe <qiaobaocn@gmail.com>
2024-10-10 10:15:22 +0800

    feature:暴力遍历，明天就要剪枝了

--- 2024/october/simple.py
+++ 2024/october/simple.py
@@ -3,7 +3,7 @@ from typing import List
 
 class SimpleSolution:
     def dest_city(self, paths: List[List[str]]) -> str:
-        paths_start =  []
+        paths_start = []
         paths_end = []
         for path in paths:
             paths_start.append(path[0])
@@ -13,9 +13,19 @@ class SimpleSolution:
                 paths_end.remove(start)
         return paths_end[0]
 
+    def number_of_pairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        ans = 0
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 % (n2 * k) == 0:
+                    ans += 1
+        return ans
+
+
 def main():
     solution = SimpleSolution()
-    print(solution.dest_city([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]))
+    print(solution.number_of_pairs([1, 2, 3, 4], [1, 2, 3, 4], 3))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
