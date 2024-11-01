9f960d3eaafa7d5a1a13ea70073c1e9ebbb83f52
majorjoe <qiaobaocn@gmail.com>
2024-11-01 18:16:11 +0800

    feature:和上一轮相关，但是状态不止一个

--- 2024/october/middle.py
+++ 2024/october/middle.py
@@ -1,17 +1,30 @@
+from typing import List
+
+
 class MiddleSolution:
 
-    def nth_person_gets_nth_seat(self, n: int) -> float:
-        # 其实只要想一下，n个座一共会有2^(n-1)种可能，然后最后一个座不被占的话，就是当最后一个座不存在，
-        # 前面随便他们怎么坐，所以其实就相当于n-1个座一共有多少种情况，也就是2^(n-2)种，所以可能性就是0.5了，
-        # 但是n-2>=0，所以1得单拿出来讨论
-        if n == 1:
-            return 1.0
-        return 0.5
+    def max_energy_boost(self, energy_drink_a: List[int], energy_drink_b: List[int]) -> int:
+        a,b = energy_drink_a,energy_drink_b
+        n = len(a)
+        dp = [[0,0] for _ in range(n+1) ]
+
+        for i in range(1,n+1):
+            if i == 1:
+                dp[1][0] = a[0]
+                dp[1][1] = b[0]
+            elif i == 2:
+                dp[2][0] = max(dp[1][0] + a[1],dp[1][1])
+                dp[2][1] = max(dp[1][1] + b[1],dp[1][0])
+            else:
+                dp[i][0] = max(dp[i-1][0] + a[i-1],dp[i-2][1] + a[i-1])
+                dp[i][1] = max(dp[i-1][1] + b[i-1],dp[i-2][0] + b[i-1])
+
+        return max(dp[n][1],dp[n][0])
 
 
 def main():
     solution = MiddleSolution()
-    print(solution.nth_person_gets_nth_seat(1))
+    print(solution.max_energy_boost([4,1,1],[1,1,3]))
 
 
 if __name__ == "__main__":
