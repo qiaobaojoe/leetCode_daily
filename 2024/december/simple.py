245d0019c258515859f5ef6502a7726b94a71d0a
majorjoe <qiaobaocn@gmail.com>
2024-12-09 14:55:54 +0800

    feature:异或运算

--- 2024/december/simple.py
+++ 2024/december/simple.py
@@ -24,9 +24,9 @@ class Solution:
     def num_rook_captures(self, board: List[List[str]]) -> int:
         ans = 0
         # 找到R的位置
-        r_i ,r_j = 0,0
-        for (i,row) in enumerate(board):
-            for (j,val) in enumerate(row):
+        r_i, r_j = 0, 0
+        for i, row in enumerate(board):
+            for j, val in enumerate(row):
                 if "R" == val:
                     r_i = i
                     r_j = j
@@ -40,7 +40,7 @@ class Solution:
             if "B" == cur_row[f]:
                 break
         # 后
-        for b in range(r_j , len(cur_row)):
+        for b in range(r_j, len(cur_row)):
             if "p" == cur_row[b]:
                 ans += 1
                 break
@@ -48,16 +48,16 @@ class Solution:
                 break
         # 上
         cur_line = []
-        for (i,_) in enumerate(board):
+        for i, _ in enumerate(board):
             cur_line.append(board[i][r_j])
-        for u in range(r_j -1 , -1, -1):
+        for u in range(r_j - 1, -1, -1):
             if "p" == cur_line[u]:
                 ans += 1
                 break
             if "B" == cur_line[u]:
                 break
         # 下
-        for d in range(r_j , len(cur_line)):
+        for d in range(r_j, len(cur_line)):
             if "p" == cur_line[d]:
                 ans += 1
                 break
@@ -65,23 +65,16 @@ class Solution:
                 break
         return ans
 
+    def square_is_white(self, coordinates: str) -> bool:
+        row = int(coordinates[1])
+        col = int(ord(coordinates[0])) - 96
+        return (row ^ col) & 1 == 1
+
 
 def main():
     solution = Solution()
-    print(
-        solution.num_rook_captures(
-            [
-                [".", ".", ".", ".", ".", ".", ".", "."],
-                [".", ".", ".", "p", ".", ".", ".", "."],
-                [".", ".", ".", "R", ".", ".", ".", "p"],
-                [".", ".", ".", ".", ".", ".", ".", "."],
-                [".", ".", ".", ".", ".", ".", ".", "."],
-                [".", ".", ".", "p", ".", ".", ".", "."],
-                [".", ".", ".", ".", ".", ".", ".", "."],
-                [".", ".", ".", ".", ".", ".", ".", "."],
-            ]
-        )
-    )
+    print(solution.square_is_white("h3"))
+    print(solution.square_is_white("a1"))
 
 
 if __name__ == "__main__":
