f316d29a5b5fd2c6df2dcf92d86f3211b2978e86
majorjoe <qiaobaocn@gmail.com>
2024-12-09 11:27:43 +0800

    feature:pthon自带的缓存装饰器

--- 2024/december/middle.py
+++ 2024/december/middle.py
@@ -1,15 +1,14 @@
-class MiddleSolution:
-    def __init__(self) -> None:
-        self.memo = {}
+from functools import cache
 
+
+class MiddleSolution:
+
+    @cache
     def knight_probability(self, n: int, k: int, row: int, column: int) -> float:
-        if self.memo.get((k, row, column)):
-            return self.memo.get((k, row, column))
+
         if row < 0 or row >= n or column < 0 or column >= n:
-            self.memo[(k, row, column)] = 0
             return 0
         if k == 0:
-            self.memo[(k, row, column)] = 1
             return 1
         probablility = 0
 
@@ -27,14 +26,13 @@ class MiddleSolution:
         probablility += self.knight_probability(n, k - 1, row + 1, column - 2) / 8
         probablility += self.knight_probability(n, k - 1, row + 1, column + 2) / 8
 
-        self.memo[(k, row, column)] = probablility
         return probablility
 
 
 def main():
     solution = MiddleSolution()
     print(solution.knight_probability(3, 2, 0, 0))
-    # print(solution.knight_probability(8, 30, 6, 4))
+    print(solution.knight_probability(8, 30, 6, 4))
 
 
 if __name__ == "__main__":
