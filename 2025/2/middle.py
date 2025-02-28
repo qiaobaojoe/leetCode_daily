4858e659b0dead5562e3f923c31981aeb0af348d
majorjoe <qiaobaocn@gmail.com>
2025-02-28 16:35:21 +0800

    features:最简单的方法，超出时间限制了，要使用数据结构

--- 2025/2/middle.py
+++ 2025/2/middle.py
@@ -19,7 +19,48 @@ class Solution:
         return ans
 
 
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.foods = foods
+        self.cuisines = cuisines
+        self.ratings = ratings
+
+    def change_rating(self, food: str, new_rating: int) -> None:
+        for i, f in enumerate(self.foods):
+            if f == food:
+                self.ratings[i] = new_rating
+                return
+
+    def highest_rated(self, cuisine: str) -> str:
+        ans_c = []
+        for i, c in enumerate(self.cuisines):
+            if c == cuisine:
+                ans_c.append(i)
+        if not ans_c:
+            return ""
+        max_rating = 0
+        ans = ""
+        for i in ans_c:
+            if self.ratings[i] > max_rating:
+                max_rating = self.ratings[i]
+                ans = self.foods[i]
+            if self.ratings[i] == max_rating:
+                if self.foods[i] < ans:
+                    ans = self.foods[i]
+        return ans
+
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
+
+
 # 主程序入口
 if __name__ == "__main__":
-    solution = Solution()
-    print(solution.max_distance([[1, 2, 3], [4, 5], [1, 2, 3]]))
+    food = FoodRatings(
+        ["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"],
+        ["korean", "japanese", "japanese", "greek", "japanese", "korean"],
+        [9, 12, 8, 15, 14, 7],
+    )
