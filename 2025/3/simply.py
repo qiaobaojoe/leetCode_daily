4e4dcf8bf0384b4ad0fd5fe94baf909925daef8f
majorjoe <qiaobaocn@gmail.com>
2025-03-29 09:56:10 +0800

    feature:没搞定

--- 2025/3/simply.py
+++ 2025/3/simply.py
@@ -37,11 +37,28 @@ class Solution:
             cost += min(i + 1, len(s) - i - 1)
         return cost
 
+    def can_be_valid(self, s: str, locked: str) -> bool:
+        if len(s) % 2:
+            return False
+        mn,mx = 0, 0
+        for b, lock in zip(s, locked):
+            if lock == '1':  # 不能改
+                d = 1 if b == '(' else -1
+                mx += d
+                if mx < 0:  # c 不能为负
+                    return False
+                mn += d
+            else:  # 可以改
+                mx += 1  # 改成左括号，c 加一
+                mn -= 1  # 改成右括号，c 减一
+            if mn < 0:  # c 不能为负
+                mn = 1  # 此时 c 的取值范围都是奇数，最小的奇数是 1
+        return mn == 0  # 说明最终 c 能是 0
+
 
 def main():
     s = Solution()
-    print(s.minimum_cost("0011"))
-    print(s.minimum_cost("010101"))
+    print(s.can_be_valid("(((())", "111111"))
 
 
 if __name__ == "__main__":
