e1953091d4da6d75693e0d38a4bd0424b51a266c
majorjoe <qiaobaocn@gmail.com>
2025-05-08 11:25:26 +0800

    featue:又复习了一遍最短路径算法，确实精妙

--- 2025/5/middle.py
+++ 2025/5/middle.py
@@ -3,10 +3,11 @@ import heapq
 
 
 class State:
-    def __init__(self, x, y, dis):
+    def __init__(self, x, y, dis, steps=0):
         self.x = x
         self.y = y
         self.dis = dis
+        self.steps = steps
 
     def __lt__(self, other):
         return self.dis < other.dis
@@ -82,11 +83,38 @@ class Solution:
 
         return dists[n - 1][m - 1]
 
+    def min_time_to_reach_v2(self, move_time: List[List[int]]) -> int:
+        n = len(move_time)  # y的长度
+        m = len(move_time[0])  # x的长度
+        dists = [[float("inf")] * m for _ in range(n)]
+        vistied = [[0] * m for _ in range(n)]
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        dists[0][0] = 0
+        heap = [State(0, 0, 0)]
+
+        while heap:
+            s = heapq.heappop(heap)
+            x, y, d, steps = s.x, s.y, s.dis, s.steps
+            if vistied[y][x] == 1:
+                continue
+            vistied[y][x] = 1
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if not (0 <= nx < m and 0 <= ny < n):
+                    continue
+                cur_d = max(d, move_time[ny][nx]) + 1 + (0 if steps % 2 == 0 else 1)
+                if cur_d < dists[ny][nx]:
+                    heapq.heappush(heap, State(nx, ny, cur_d, steps + 1))
+                    dists[ny][nx] = cur_d
+        return dists[n - 1][m - 1]
+
 
 def main():
     s = Solution()
     # print(s.network_delay_time([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2))
-    print(s.min_time_to_reach([[94, 79, 62, 27, 69, 84], [6, 32, 11, 82, 42, 30]]))
+    print(s.min_time_to_reach_v2([[0, 4], [4, 4]]))
+    # print(s.min_time_to_reach([[0, 4], [4, 4]]))
 
 
 if __name__ == "__main__":
