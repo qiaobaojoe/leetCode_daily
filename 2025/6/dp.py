6f72c0a3a852274f287f0412f7656cf4fdce6bf1
majorjoe <qiaobaocn@gmail.com>
2025-06-03 11:15:00 +0800

    feature:简单dp

--- 2025/6/dp.py
+++ 2025/6/dp.py
@@ -33,7 +33,22 @@ class DpQuestion:
     def delete_and_earn_test(self):
         print(self.delete_and_earn([3, 4, 2]))
 
+    def unique_paths(self, m: int, n: int) -> int:
+        # 很直接的dp思路 f[m][n] = f[m-1][n] + f[m][n-1]
+        dp = [[0] * n for _ in range(m)]
+        for y in range(1, m):
+            dp[y][0] = 1
+        for x in range(1, n):
+            dp[0][x] = 1
+        for y in range(1, m):
+            for x in range(1, n):
+                dp[y][x] = dp[y - 1][x] + dp[y][x - 1]
+        return dp[m-1][n-1]
+
+    def unique_paths_test(self):
+        print(self.unique_paths(3, 7))
+
 
 if __name__ == "__main__":
     s = DpQuestion()
-    s.delete_and_earn_test()
+    s.unique_paths_test()
