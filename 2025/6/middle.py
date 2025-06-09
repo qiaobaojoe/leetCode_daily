6054b9215414708fbc8f4d46cfc1db12babc9bb3
majorjoe <qiaobaocn@gmail.com>
2025-06-09 18:43:32 +0800

    feature:很有成就感

--- 2025/6/middle.py
+++ 2025/6/middle.py
@@ -1,5 +1,6 @@
 from collections import deque
 from string import ascii_lowercase
+from typing import List
 
 
 class Solution:
@@ -46,7 +47,7 @@ class Solution:
 
     def clear_stars(self, s: str) -> str:
         stack = [[] for _ in range(26)]
-        for i,c in enumerate(s):
+        for i, c in enumerate(s):
             if c == "*":
                 for c_s in stack:
                     if c_s:
@@ -54,20 +55,79 @@ class Solution:
                         break
             else:
                 stack[ord(c) - ord("a")].append(i)
-        ans = [''] * len(s)
-        for index,c_s in enumerate(stack):
+        ans = [""] * len(s)
+        for index, c_s in enumerate(stack):
             if c_s:
                 for i in c_s:
-                    ans[i] = chr(index+ord('a'))
-        return ''.join(ans)
+                    ans[i] = chr(index + ord("a"))
+        return "".join(ans)
 
     def clear_stars_test(self):
         print(self.clear_stars("aaba*"))
 
+    def longest_palindrome(self, s: str) -> str:
+        # 子字符串为最长回文字符串的最大长度一题
+        # 确认边界条件 1<= len
+        # 长度为1的字段都是回文字符串
+        n = len(s)
+        if n == 1:
+            return s[0]
+        # 长度为2的回文字符串只有可能是两个字符相等
+        # 我的思路 遍历找到 长度为2的回文字符串和所有长度为1的字符串（都为回文字符串），以这些节点为启动，判断左右字符是否相等，相等就是组成新的回文字符串，再次参与迭代，
+        # 中心发散的思路，有些类型深度搜索，但是每个节点的迭代发散都是独立的，没有剪枝
+        # 哈哈 看了题解，我这么做就可以了 这里官方给出题解是递归的方式，但是我刚才的思路，应该迭代更直接一点
+        palindrome_nodes = deque()
+        for i in range(n):
+            palindrome_nodes.append((i, i))
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                palindrome_nodes.append((i, i + 1))
+        max_len = 0
+        start_i = 0
+        while palindrome_nodes:
+            (l, r) = palindrome_nodes.popleft()
+            if max_len < (r - l + 1):
+                max_len = r - l + 1
+                start_i = l
+            if l == 0 or r == n - 1:
+                continue
+            if s[l - 1] == s[r + 1]:
+                palindrome_nodes.append((l - 1, r + 1))
+
+        return s[start_i : start_i + max_len]
+
+    def longest_palindrome_test(self):
+        print(self.longest_palindrome("cbbd"))
+
+    def word_break(self, s: str, word_dict: List[str]) -> bool:
+        n = len(s)
+        sub_grid = [[0] * n for _ in range(n)]
+        for word in word_dict:
+            m = len(word)
+            if m > n:
+                continue
+            for i in range(n - m + 1):
+                sub = s[i : i + m]
+                if sub == word:
+                    sub_grid[i][i + m - 1] = 1
+        # 到这一步已经把题目转化成几根棍子的问题了，在这里好像可以用到动态规划的思想了
+        # 比较经典的要或者不要的场景
+        dp = [False] * (n + 1)
+        dp[0] = True
+        for i in range(n):
+            for (j,arrived) in enumerate(sub_grid[i]):
+                if arrived:
+                    dp[j+1] =  dp[j+1] | dp[i]
+        return dp[n]
+
+
+    def word_break_test(self):
+        print(self.word_break("aaaaaaa", ["aaaa","aaa"]))
+
 
 def main():
     s = Solution()
-    s.clear_stars_test()
+    s.word_break_test()
 
 
 if __name__ == "__main__":
